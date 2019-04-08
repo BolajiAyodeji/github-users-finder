5bef0b96482bd68d2d7581f91e08eac744671b5e
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-04-08 11:09:30 +0100

    revamp ui

--- js/ui.js
+++ js/ui.js
@@ -47,20 +47,21 @@ class UI {
 
     repos.map((repo) => {
       output += `
-      <div class="ca rd card-body mb-2">
+      <div class="card card-body mb-2">
        <div class="row">
         <div class"col-md-6">
-        <a class="mr-3 mb-3 badge badge-info" href="${repo.html_url}"
+        <a class="mr-3 badge badge-info" href="${repo.html_url}"
         target="_blank">${repo.name}</a>
         </div>
+        <br>
         <div class"col-md-6">
-         <span class="badge badge-primary mb-2">
+         <span class="badge badge-dark mb-2">
           Stars: ${repo.stargazers_count}
          </span>
-         <span class="badge badge-secondary mb-2">
+         <span class="badge badge-dark mb-2">
          Watchers: ${repo.watchers_count}
          </span>
-         <span class="badge badge-success mb-2">
+         <span class="badge badge-dark mb-2">
          Forks: ${repo.forks}
          </span>
         </div>
