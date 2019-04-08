46d95921bcbf1cb49c0a8e815e5be7556b5b74ca
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-04-08 11:22:12 +0100

    update latest repos :zap:

--- js/github.js
+++ js/github.js
@@ -3,7 +3,7 @@ class GitHub {
   constructor()  {
     this.client_id = 'e9a3759e4c0e7644d72a';
     this.client_secret = '9fd64cdc96ecb9f12ca8f3134ec616d62a82b8d4';
-    this.repos_count = 5;
+    this.repos_count = 10;
     this.repos_sort = 'created:asc';
   }
 
